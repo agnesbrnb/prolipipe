c3b37e094ce3dc8738fe126cc0e4d96ad5829101
ytirlet <101868688+ytirlet@users.noreply.github.com>
2022-12-20 13:37:11 +0100

    Update rename.py

--- rename.py
+++ rename.py
@@ -3,7 +3,6 @@ import os
 
 
 list = os.listdir('/home/ytirlet/Documents/yael/prokka_pub/')
-#list = ['Lactobacillus_rhamnosus_GG_(ATCC_53103)_Complete','Lactobacillus_brevis_15f_Complete','GC:F_0.094.95475.1','Pediococcus_pentosaceus_LMG_11487_Scaffold','P_freudenreichii_CIRM_BIA118']
 dico_prefix = {'Bifidobacterium':'B','Lactobacillus':'Lb', 'L':'Lco', 'Lactococcus':'Lco',  'Leuconostoc':'Leu', 'Pediococcus':'Pe', 'Propionibacterium':'Pr', 'P':'Pr', 'Streptococcus':'St'}
 dico_suffix = {'Complete':'C', 'complet':'C', 'C':'C', 'Scaffold':'S', 'S':'S', 'Plasmid':'P', 'plasmide':'P', 'P':'P'}
 for name in list :
@@ -25,9 +24,6 @@ for name in list :
         while '.' in new_name or ':' in new_name :
             new_name = new_name.replace('.','-')
             new_name = new_name.replace(':','-')
-    # print(name)
-    # print(new_name)
-    # print('___________')
     os.system("mv -fv /home/ytirlet/Documents/yael/prokka_pub/"+name+"/*.gbk /home/ytirlet/Documents/yael/prokka_pub/"+name+"/"+new_name+".gbk")
     os.system("mv -fv /home/ytirlet/Documents/yael/prokka_pub/"+name+"/ /home/ytirlet/Documents/yael/prokka_pub/"+new_name+"/")
 
